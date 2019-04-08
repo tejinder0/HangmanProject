024bbe1516cfabef249eb1783132ce3024a1001c
Husandeep Singh Hayer <HH026@atcltd.atc.org.nz>
2019-04-08 13:43:27 +1200

    Add project files.

---
+++ Hangman/MainActivity.cs
@@ -0,0 +1,77 @@
+﻿
+using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Hangman.Assets;
+using Hangman.Resources;
+using Java.Security;
+
+namespace Hangman
+{
+    [Activity(Label = "Hangman", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private Button btnNewGameScreen;
+        private Button btnHighScores;
+       
+       // private Button btnQuit;
+     
+        
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+          
+
+            btnNewGameScreen = FindViewById<Button>(Resource.Id.btnNewGameScreen);
+            btnNewGameScreen.Click += btnNewGameScreen_Click;
+
+            btnHighScores = FindViewById<Button>(Resource.Id.btnHighScores);
+            btnHighScores.Click += btnHighScores_Click;
+
+            Button btnEditDB = FindViewById<Button>(Resource.Id.btnEditDB);
+            btnEditDB.Click += btnEditDB_Click;
+
+            Button btnQuit = FindViewById<Button>(Resource.Id.btnQuit);
+            btnQuit.Click += btnQuit_Click;
+
+
+
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            //System.Environment.Exit(0);
+            var activity = (Activity)this;
+            activity.FinishAffinity();
+        }
+
+        private void btnEditDB_Click(object sender, EventArgs e)
+        {
+            StartActivity(typeof(EditDB));
+        }
+
+
+    
+
+      
+
+        private void btnHighScores_Click(object sender, EventArgs e)
+        {
+          StartActivity(typeof(HighScores));
+        }
+
+        private void btnNewGameScreen_Click(object sender, EventArgs e)
+        {
+            StartActivity(typeof(EnterPlayerName));
+        }
+    }
+}
+
---
+++ Hangman/Resources/HangmanQuestion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+namespace Hangman.Resources
+{
+    [Table("HangmanQuestions")]
+    public class HangmanQuestion
+    {
+        public string Question { get; set; }
+
+        public string Answer { get; set; }
+    }
+}
